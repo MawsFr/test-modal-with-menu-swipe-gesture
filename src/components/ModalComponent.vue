860ece4f8a47398cc2a4f897a226df12c667899e
Mustapha NEZZARI <mnezzari@partner.auchan.fr>
2022-02-18 23:54:06 +0100

    Add workaround

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -1,16 +1,20 @@
 <template>
   <ion-modal
+    ref="modal"
     :is-open="isOpen"
     :breakpoints="[0, 0.5, 1]"
     :initialBreakpoint="1"
     @ionModalDidDismiss="onDidDismiss"
   >
     <ion-button @click="closeModal">Close modal</ion-button>
+    <ion-button @click="closeModalAndRemoveShowModalClass"
+      >Close modal and remove show-modal class</ion-button
+    >
   </ion-modal>
 </template>
 <script lang="ts">
 import { IonModal, modalController, IonButton } from "@ionic/vue";
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 export default defineComponent({
   components: {
     IonModal,
@@ -23,13 +27,30 @@ export default defineComponent({
     },
   },
   setup(_props, { emit }) {
+    const modal = ref();
+    const doBug = ref(false);
     const closeModal = () => {
+      doBug.value = true;
       modalController.dismiss();
     };
+
+    const closeModalAndRemoveShowModalClass = () => {
+      doBug.value = false;
+      modalController.dismiss();
+    };
+
     const onDidDismiss = () => {
+      if (!doBug.value) {
+        modal.value.$el.classList.remove("show-modal");
+      }
       emit("update:isOpen", false);
     };
-    return { closeModal, onDidDismiss };
+    return {
+      modal,
+      closeModal,
+      closeModalAndRemoveShowModalClass,
+      onDidDismiss,
+    };
   },
 });
 </script>
